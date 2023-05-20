00fa05415d7cadcbf1cfdad3634ca7b14241868f
fpcoutinho <fpcoutinho5@gmail.com>
2023-05-20 14:27:37 -0300

    esqueeleto de teste

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupDasRotasDeTeste() *gin.Engine {
+	rotas := gin.Default()
+	return rotas
+}
+
+func TestMain(t *testing.T) {
+	t.Fatalf("not implemented")
+}
